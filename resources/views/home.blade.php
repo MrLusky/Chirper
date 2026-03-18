120f077d122de3abee5ef83023898e5c2e262d80
MrLusky <lucasdanielpereira2008@gmail.com>
2026-03-18 04:50:11 -0300

    uma série de mudanças que tornaram o código algo mais profissional

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,15 +3,26 @@
         Welcome
     </x-slot:title>
     <div class="max-w-2xl mx-auto">
+        @foreach ($chirps as $chirps) {{-- É assim que se faz um comentário no blade, aliás, ali diz que para cada chirp no array, ela vai interagir com o que tiver abaixo --}}
         <div class="card bg-base-100 shadow mt-8">
             <div class="card-body">
                 <div>
-                    <h1 class="text-3xl font-bold">Welcome to Chirper!</h1>
-                    <p class="mt-4 text-base-content/60">This is your brand new Laravel application. Time to make it
-                        sing (or chirp)!</p>
-                        <p class="mt-2 text-sm text-gray-600">Now this is on your GitHub! 🎉, por que estou fazendo tudo isso em inglês?"</p>
+                    <div class="font-semibold">{{ $chirps['author'] }}</div>
+                    <div class="mt-1">{{ $chirps['message'] }}</div>
+                    <div class="text-sm text-gray-500 mt-2">{{ $chirps['time'] }}</div>
                 </div>
             </div>
         </div>
+        @endforeach
     </div>
 </x-layout>
+
+
+{{--
+
+<h1 class="text-3xl font-bold">Welcome to Chirper!</h1>
+<p class="mt-4 text-base-content/60">This is your brand new Laravel application. Time to make it
+    sing (or chirp)!</p>
+    <p class="mt-2 text-sm text-gray-600">Now this is on your GitHub! 🎉, por que estou fazendo tudo isso em inglês?"</p>
+tava assim antes dentro daquele div dps do "card-body"
+--}}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\ChirpController;
 use Illuminate\Support\Facades\Route;
+
+/*Notas: o / significa a homepage (primeira coisa que vê quando acessa), e se comparado ao que tava antes, eu só deixei mais limpo:
 
 Route::get('/', function () {
     return view('home');
-});
+}
+
+Isso era o que tava antes
+*/
+Route::get('/', [ChirpController::class, 'index']);
